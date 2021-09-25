c05df13a7a7198008f44643226c8b2f1fb9ecd5f
goelabhishek694 <goelabhishek694@gmail.com>
2021-09-25 16:59:08 +0530

    Added upto crud

--- foodApp/app.js
+++ foodApp/app.js
@@ -1,24 +1,24 @@
 const express = require("express");
-
 const app = express();
+const mongoose=require('mongoose');
 //middleware func-> post, front-> json
 app.use(express.json()); //global middleware 
 app.listen(3000);
 
-let users = [
-  {
-    id: 1,
-    name: "Abhishek",
-  },
-  {
-    id: 2,
-    name: "Jasbir",
-  },
-  {
-    id: 3,
-    name: "Kartik",
-  },
-];
+// let users = [
+//   {
+//     id: 1,
+//     name: "Abhishek",
+//   },
+//   {
+//     id: 2,
+//     name: "Jasbir",
+//   },
+//   {
+//     id: 3,
+//     name: "Kartik",
+//   },
+// ];
 //mini app
 const userRouter = express.Router();
 const authRouter=express.Router();
@@ -28,7 +28,7 @@ app.use("/auth", authRouter);
 
 userRouter
   .route("/")
-  .get(getUser) //path specific middleware
+  .get(getUsers) //path specific middleware
   .post(postUser)
   .patch(updateUser)
   .delete(deleteUser);
@@ -40,9 +40,12 @@ authRouter
 .get(middleware1,getSignUp,middleware2)
 .post(postSignUp);
 
-function getUser(req, res) {
-  console.log(req.query);
-  res.send(users);
+async function getUsers(req, res) {
+  // console.log(req.query);
+  // let allUsers=await userModel.find();
+  let user=await userModel.findOne({name:'Abhishek'});
+  res.json({message:'list of all users',
+  data:allUsers});
 }
 
 function postUser(req, res) {
@@ -54,22 +57,27 @@ function postUser(req, res) {
   });
 }
 
-function updateUser(req, res) {
+async function updateUser(req, res) {
   console.log("req.body-> ", req.body);
   //update data in users obj
   let dataToBeUpdated = req.body;
-  for (key in dataToBeUpdated) {
-    users[key] = dataToBeUpdated[key];
-  }
+  let user= await userModel.findOneAndUpdate({email:'abc@gmail.com'},dataToBeUpdated);
+  // for (key in dataToBeUpdated) {
+  //   users[key] = dataToBeUpdated[key];
+  // }
   res.json({
     message: "data updated successfully",
+    data:user
   });
 }
 
-function deleteUser(req, res) {
-  users = {};
+async function deleteUser(req, res) {
+  // users = {};
+  let dataToBeDeleted=req.body;
+  let user=await userModel.findOneAndDelete(dataToBeDeleted);
   res.json({
     message: "data has been deleted",
+    data:user
   });
 }
 
@@ -97,11 +105,61 @@ function getSignUp(req,res,next){
   // res.sendFile('/public/index.html',{root:__dirname});
 }
 
-function postSignUp(req,res){
-  let obj=req.body;
-  console.log('backend',obj);
+async function postSignUp(req,res){
+  let dataObj=req.body;
+  let user=await userModel.create(dataObj);
+  console.log('backend',user);
   res.json({
     message:"user signed up",
-    data:obj
+    data:user
   });
 }
+
+
+//mongoDB
+
+const db_link='mongodb+srv://admin:xnDx4jlj5mmzjiVE@cluster0.3irmz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+mongoose.connect(db_link)
+.then(function(db){
+  // console.log(db);
+  console.log('db connected');
+})
+.catch(function(err){
+  console.log(err);
+});
+
+const userSchema=mongoose.Schema({
+  name:{
+    type:String,
+    required:true
+  },
+  email:{
+    type:String,
+    required:true,
+    unique:true
+  },
+  password:{
+    type:String,
+    required:true,
+    minLength:8
+  },
+  confirmPassword:{
+    type:String,
+    required:true,
+    minLength:8
+  }
+});
+
+// model
+const userModel=mongoose.model('userModel',userSchema);
+
+// (async function createUser(){
+//   let user={
+//     name:'Jasbir',
+//     email:'abcd@gmail.com',
+//     password:'12345678',
+//     confirmPassword:'12345678'
+//   };
+//   let data= await userModel.create(user);
+//   console.log(data);
+// })();
